283d135a344f08691c4b032df7505d8a6a9eaa86
sandhiyaindu <51949598+sandhiyaindu@users.noreply.github.com>
2019-06-25 14:19:41 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,7 +1,8 @@
 a=int(input("enter the number:"))
-if (a%2)==0:
-  print("this number is even")
-elif(a<0):
-  print("invalid")
+if(n>0):
+  if (a%2)==0:
+    print("Even")
+  else:
+    print("Odd")
 else:
-  print("this number is odd")
+  print("invalid")
