1fb5b00b45c101e7920532721e2037881c90664f
sandhiyaindu <51949598+sandhiyaindu@users.noreply.github.com>
2019-07-01 15:04:31 +0530

    Create consonant.py

---
+++ consonant.py
@@ -0,0 +1,7 @@
+ch=input()
+if ch in('a','e','i','o','u'):
+  print("vowel")
+elif ch in('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+  print("Consonant")
+else:
+  print("invalid")
