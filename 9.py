c6ef0d3f1884750a138b672293d4af51ccd220a1
sandhiyaindu <51949598+sandhiyaindu@users.noreply.github.com>
2019-07-03 14:52:10 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,6 +1,6 @@
-s1,s2=map(int,input().split())
-n=list(map(int,input().split())
+n1,n2=map(int,input().split())
+s=list(map(int,input().split())
 sum=0
-for i in range(s2):
-  sum=sum+n[i]
+for i in range(n2):
+  sum=sum+s[i]
 print(sum)
