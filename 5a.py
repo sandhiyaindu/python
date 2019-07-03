0f300bc15be7b0ff4158879e2e7ddcf30b8bbd8a
sandhiyaindu <51949598+sandhiyaindu@users.noreply.github.com>
2019-07-03 11:20:51 +0530

    Update 5a.py

--- 5a.py
+++ 5a.py
@@ -1,7 +1,7 @@
-x,y,z=input().split()
-if(x>=y and y>=z):
-  print(x)
-elif(y>=x and y>=z):
-  print(y)
-else:
-  print(z)
+a,b,c=input().split()
+if(a>=b and b>=c):
+  print(a)
+elif(b>=a and b>=c):
+  print(b)
+elif(c>=a and c>=b):
+  print(c)
