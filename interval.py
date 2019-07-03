5fa315167df868e98a8a681ebc5aea3fc632ed68
sandhiyaindu <51949598+sandhiyaindu@users.noreply.github.com>
2019-07-03 17:09:00 +0530

    Update interval.py

--- interval.py
+++ interval.py
@@ -1,4 +1,4 @@
-a,s=[int(x) for x in input().split()]
-for j in range(a+1,s+1):
+r,s=[int(x) for x in input().split()]
+for j in range(r+1,s+1):
     if(j%2!=0):
-        print(j,end=" ")
+        print(j,end=' ')
